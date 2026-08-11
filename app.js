d28bbf07c7f4fac0f48e2bc5cba99a706fa925fc
haotian562-ux <haotian562@icloud.com>
2026-08-12 02:37:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,155 @@
    FRONTEND SYSTEM v2.0
 ===================================================== */
 
+/* =================================
+ MATRIX DIGITAL RAIN
+================================= */
 
+
+const canvas =
+document.getElementById(
+"matrix-canvas"
+);
+
+
+
+if(canvas){
+
+
+const ctx =
+canvas.getContext(
+"2d"
+);
+
+
+
+canvas.width =
+window.innerWidth;
+
+
+canvas.height =
+window.innerHeight;
+
+
+
+const chars =
+"01アイウエオカキクケコABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+
+const fontSize=18;
+
+
+const columns =
+canvas.width/fontSize;
+
+
+
+const drops=[];
+
+
+
+for(
+let i=0;
+i<columns;
+i++
+){
+
+drops[i]=1;
+
+}
+
+
+
+function matrixRain(){
+
+
+
+ctx.fillStyle =
+"rgba(0,0,0,0.08)";
+
+
+ctx.fillRect(
+0,
+0,
+canvas.width,
+canvas.height
+);
+
+
+
+ctx.fillStyle =
+"#00ff41";
+
+
+
+ctx.font =
+fontSize+"px monospace";
+
+
+
+for(
+let i=0;
+i<drops.length;
+i++
+){
+
+
+
+const text =
+chars[
+Math.floor(
+Math.random()*chars.length
+)
+];
+
+
+
+ctx.fillText(
+
+text,
+
+i*fontSize,
+
+drops[i]*fontSize
+
+);
+
+
+
+if(
+drops[i]*fontSize >
+canvas.height
+&&
+Math.random()>0.975
+){
+
+drops[i]=0;
+
+
+}
+
+
+
+drops[i]++;
+
+
+}
+
+
+
+}
+
+
+
+setInterval(
+matrixRain,
+50
+);
+
+
+
+}
 
 document.addEventListener(
 "DOMContentLoaded",
